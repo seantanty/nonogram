ab7aa9f112335542d1ae563f5f2291fa10f86c33
XinyiJackieGe <61487236+XinyiJackieGe@users.noreply.github.com>
2021-03-07 16:48:51 -0800

    Update MyDB.js

--- db/MyDB.js
+++ db/MyDB.js
@@ -7,6 +7,48 @@ function MyDB() {
     "mongodb+srv://seantan:<password>@cluster0.u90qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   // process.env.MONGO_URL || "mongodb://localhost:27017";
   const DB_NAME = "5610Project2";
+  
+  // Get popular puzzles for displaying leader boards.
+  myDB.getPuzzles = async () => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      console.log("Connecting to the db");
+      await client.connect();
+      console.log("Connected!");
+      const db = client.db(DB_NAME);
+      const filesCol = db.collection("puzzles");
+      const files = await filesCol.aggregate([
+        {$project : {leaderBoard : 1, length : {$size : "$usersPlayed"}}}, 
+        {$sort : {length : -1}},
+        {$limit: 3}]).toArray();
+      console.log("Got puzzles", files);
+      return files;
+    } finally {
+      console.log("Closing the connection");
+      client.close();
+    }
+  };
+  
+  // get the puzzle searched by puzzle id  
+  myDB.getPuzzleById = async (query) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      console.log("Connecting to the db");
+      await client.connect();
+      console.log("Connected!");
+      const db = client.db(DB_NAME);
+      const filesCol = db.collection("puzzles");
+      console.log("Collection ready, querying with id: ", query);
+      const files = await filesCol.find({_id: ObjectId(query)}).toArray();
+      console.log("Got the puzzle", files);
+      return files[0];
+    } finally {
+      console.log("Closing the connection");
+      client.close();
+    }
+  };
 
   myDB.getFiles = async (query = {}) => {
     let client;
